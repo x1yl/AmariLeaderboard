cf9acd66f73bd029d88247b1afd81c6e48be029d
xtyl <life23243@tutanota.com>
2024-03-16 19:55:35 -0400

    w

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // app.js
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const compression = require("compression");
+const helmet = require("helmet");
 
 // Define the directory where your static files are located
 app.use(express.static(__dirname));
+app.use(compression()); // Compress all routes
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
 
 module.exports = { app, port };
