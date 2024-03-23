a349af88ade0b063440162ce816ec4208125b704
xtyl <life23243@tutanota.com>
2024-03-23 16:22:44 -0400

    fixed regex

--- guildSelection.js
+++ guildSelection.js
@@ -2,7 +2,7 @@
 document.getElementById('serverForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const server = document.getElementById('serverInput').value.trim();
-    var regex = /^\d*$/;
+    var regex = /^\d{17,}$/;
     if (server && regex.test(server)) {
         window.location = `/?server=${encodeURIComponent(server)}`;
         return;
